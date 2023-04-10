d151f536566c01a08c5470925034b94b4a2863e8
Alexander Gorsc <alexandergorsc@gmail.com>
2023-04-10 12:42:07 +0300

    create&fill_array

---
+++ prog/Program.cs
@@ -0,0 +1,23 @@
+﻿// Создаем исходный массив
+string [] array = new string[10];
+
+void FillArrayWithConsole ()
+{
+    for(int i=0;i<array.Length;i++)
+    {
+        System.Console.Write($"Введите {i+1} строку:"); 
+        array[i] = Console.ReadLine();
+    }
+}
+
+void PrintArray (string [] array)
+{
+    for(int i=0;i<array.Length;i++)
+    {  
+        Console.Write(array[i]+" | ");
+    }
+}
+
+FillArrayWithConsole();
+PrintArray (array);
+
