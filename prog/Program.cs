2e49b51fae746fe32cb58cf68e3e468a984f64cf
Alexander Gorsc <alexandergorsc@gmail.com>
2023-04-10 13:56:20 +0300

    add_findThreeSumbol_method

--- prog/Program.cs
+++ prog/Program.cs
@@ -1,23 +1,50 @@
 ﻿// Создаем исходный массив
-string [] array = new string[10];
-
-void FillArrayWithConsole ()
+string [] FillArrayWithConsole()
 {
+    int size;
+    while(true)
+    {
+    System.Console.WriteLine("Введите размер массива: ");
+    size = Int32.Parse(Console.ReadLine());
+    if (size>0) break;
+    System.Console.WriteLine("Ошибка: размер массива не может быть отрицательным");
+    }
+    string [] array = new string[size];
     for(int i=0;i<array.Length;i++)
     {
         System.Console.Write($"Введите {i+1} строку:"); 
         array[i] = Console.ReadLine();
     }
+    return array;
 }
 
-void PrintArray (string [] array)
+void PrintArray (string [] array,string text)
 {
+    System.Console.Write(text);
     for(int i=0;i<array.Length;i++)
     {  
         Console.Write(array[i]+" | ");
     }
+    System.Console.WriteLine();
 }
 
-FillArrayWithConsole();
-PrintArray (array);
+string [] FindThreeSumbolString (string [] array)
+{
+    string [] tmpArray = new string [array.Length];
+    int current =0;
+    for(int i =0;i<array.Length;i++)
+    {
+        if (array[i].Length<=3)
+        {
+        tmpArray[current] = array[i];
+        current++;
+        }
+    }
+    Array.Resize(ref tmpArray,current);
+    return tmpArray;
+}
 
+string [] array = FillArrayWithConsole();
+PrintArray (array,$"Искомый массив(Размера {array.Length}): ");
+string [] newArray = FindThreeSumbolString (array);
+PrintArray (newArray,$"Результирующий масссив(Размера {newArray.Length}): ");
